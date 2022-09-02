9a444a176279e241ca413821a8004b603a2f6057
Runjuu <hi@runjuu.com>
2022-09-03 01:18:22 +0800

    feat: create `ipfs-gateway-nextjs` example

---
+++ examples/ipfs-gateway-nextjs/next.config.js
@@ -0,0 +1,9 @@
+const { withIpfsGateway } = require('@crossbell/ipfs-gateway-next')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = withIpfsGateway(nextConfig)
