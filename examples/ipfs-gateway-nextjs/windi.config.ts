b8a546605ffa009bd209041b5d0326d3bf0d0d48
Runjuu <hi@runjuu.com>
2022-09-04 01:04:28 +0800

    fix: build issues

--- examples/ipfs-gateway-nextjs/windi.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'windicss/helpers'
-
-export default defineConfig({
-  extract: {
-    include: ['**/*.{tsx,css}'],
-    exclude: ['node_modules', '.git', '.next/**/*', '.turbo/**/*'],
-  },
-})
