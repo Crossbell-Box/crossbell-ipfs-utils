4cde4cc3bbda30cd79bf03c75cbc9d9ff4828505
Runjuu <hi@runjuu.com>
2022-09-03 20:41:56 +0800

    feat(ipfs-gateway-next): add options to enable sever gateway

--- packages/ipfs-gateway-next/src/index.ts
+++ packages/ipfs-gateway-next/src/index.ts
@@ -1,25 +1,34 @@
 import type { NextConfig } from 'next'
-import type { Rewrite } from 'next/dist/lib/load-custom-routes'
+import type { Rewrite, Redirect } from 'next/dist/lib/load-custom-routes'
+
 import CopyPlugin from 'copy-webpack-plugin'
+import { DEFAULT_IPFS_GATEWAYS, fillIpfsGatewayTemplate, Web2Url } from '@crossbell/ipfs-fetch'
+
+type GatewayPath = `/${string}/:cid/:pathToResource*`
+type FallbackGateway = `${Web2Url}:cid${string}:pathToResource*`
 
 export type WithIpfsGatewayConfig = NextConfig & {
   ipfsGateway?: Partial<{
-    gatewayPath: `/${string}/:path*`
-    fallbackGateway: `https://${string}/ipfs/:path*`
+    gatewayPath: GatewayPath
+    fallbackGateway: FallbackGateway
     serviceWorkerFilename: string
+    enableServerGateway: boolean
   }>
 }
+
+const defaultFallbackGateway = fillIpfsGatewayTemplate(DEFAULT_IPFS_GATEWAYS[1], {
+  cid: ':cid',
+  pathToResource: ':pathToResource*',
+}) as FallbackGateway
 
 export function withIpfsGateway({
   ipfsGateway,
   ...userConfig
 }: WithIpfsGatewayConfig = {}): NextConfig {
-  const {
-    gatewayPath = '/ipfs/:path*',
-    fallbackGateway = 'https://cloudflare-ipfs.com/ipfs/:path*',
-    serviceWorkerFilename = 'ipfs-gateway-sw.js',
-  } = ipfsGateway ?? {}
-
+  const gatewayPath: GatewayPath = ipfsGateway?.gatewayPath ?? '/ipfs/:cid/:pathToResource*'
+  const fallbackGateway: FallbackGateway = ipfsGateway?.fallbackGateway ?? defaultFallbackGateway
+  const serviceWorkerFilename = ipfsGateway?.serviceWorkerFilename ?? 'ipfs-gateway-sw.js'
+  const enableServerGateway = ipfsGateway?.enableServerGateway ?? false
   const swFilePath = `static/${serviceWorkerFilename}`
 
   return {
@@ -40,24 +49,34 @@ export function withIpfsGateway({
 
     async rewrites(...params) {
       const userRewrites = (await userConfig.rewrites?.(...params)) ?? []
-      const rewrite: Rewrite[] = [
-        {
-          source: `/${serviceWorkerFilename}`,
-          destination: `/_next/${swFilePath}`,
-          locale: false,
-        },
-        {
+      const rewrite: Rewrite = {
+        source: `/${serviceWorkerFilename}`,
+        destination: `/_next/${swFilePath}`,
+        locale: false,
+      }
+
+      if (Array.isArray(userRewrites)) {
+        return [...userRewrites, rewrite]
+      } else {
+        return { ...userRewrites, fallback: [...(userRewrites.fallback ?? []), rewrite] }
+      }
+    },
+
+    async redirects(...params) {
+      const userRedirects = (await userConfig.redirects?.(...params)) ?? []
+
+      if (enableServerGateway) {
+        const redirect: Redirect = {
+          permanent: false,
           // TODO: - Validate `gatewayPath` pattern
           source: gatewayPath,
           // TODO: - Validate `fallbackGateway` pattern
           destination: fallbackGateway,
-        },
-      ]
+        }
 
-      if (Array.isArray(userRewrites)) {
-        return [...userRewrites, ...rewrite]
+        return [...userRedirects, redirect]
       } else {
-        return { ...userRewrites, fallback: [...(userRewrites.fallback ?? []), ...rewrite] }
+        return userRedirects
       }
     },
   }
