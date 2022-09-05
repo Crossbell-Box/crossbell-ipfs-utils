8a6a8f1db3610ab829054aa1cd443f3e588ee235
Runjuu <hi@runjuu.com>
2022-09-05 19:20:43 +0800

    feat(ipfs-gateway): update `registerServiceWorker`
    wait until sevice worker completely ready.
    
    Firefox emit `navigator.serviceWorker.ready` event early at first time install

--- packages/ipfs-gateway/src/sw-register.ts
+++ packages/ipfs-gateway/src/sw-register.ts
@@ -1,6 +1,7 @@
-import qs from 'query-string'
 import type { IpfsGatewayTemplate } from '@crossbell/ipfs-fetch'
 import type { GatewayPrefix, ServiceWorkerFilename } from '@crossbell/ipfs-gateway-sw'
+import { CHECK_IPFS_GATEWAY_SW_STATUS, IPFS_GATEWAY_SW_IS_UP } from '@crossbell/ipfs-gateway-sw'
+import qs from 'query-string'
 
 export type RegisterServiceWorkerConfig = {
   gatewayPrefix: GatewayPrefix
@@ -16,5 +17,22 @@ export function registerServiceWorker({
     scope: '/',
   })
 
-  return navigator.serviceWorker.ready
+  navigator.serviceWorker.ready.then((registration) => {
+    return registration.active?.postMessage(CHECK_IPFS_GATEWAY_SW_STATUS)
+  })
+
+  return waitUntilServiceWorkerUp()
+}
+
+function waitUntilServiceWorkerUp() {
+  return new Promise<void>((resolve) => {
+    const onMessage = (event: MessageEvent) => {
+      if (event.data === IPFS_GATEWAY_SW_IS_UP) {
+        navigator.serviceWorker.removeEventListener('message', onMessage)
+        resolve()
+      }
+    }
+
+    navigator.serviceWorker.addEventListener('message', onMessage)
+  })
 }
