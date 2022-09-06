5f0618b61996b0a5173425ccf2e8582971bfe9e6
Runjuu <hi@runjuu.com>
2022-09-06 10:08:13 +0800

    feat(ipfs-fetch): update DEFAULT_IPFS_GATEWAYS

--- packages/ipfs-fetch/src/constant.ts
+++ packages/ipfs-fetch/src/constant.ts
@@ -1,9 +1,9 @@
 import { IpfsGatewayTemplate } from './types'
 
 export const DEFAULT_IPFS_GATEWAYS: IpfsGatewayTemplate[] = [
+  'https://{cid}.ipfs.cf-ipfs.com/{pathToResource}',
   'https://ipfs.io/ipfs/{cid}{pathToResource}',
   'https://{cid}.ipfs.dweb.link/{pathToResource}',
   'https://cloudflare-ipfs.com/ipfs/{cid}{pathToResource}',
-  'https://{cid}.ipfs.cf-ipfs.com/{pathToResource}',
   'https://4everland.xyz/ipfs/{cid}{pathToResource}',
 ]
